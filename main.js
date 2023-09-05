edee9d6ca501a20663dc88f8de9414a48e35ef17
rosvm0610 <102706567+rosvm0610@users.noreply.github.com>
2023-09-04 20:11:59 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+function suena1() {
+  document.getElementById("audio1").play();
+}
+
+function suena2() {
+  document.getElementById("audio2").play();
+}
+
+function suena3() {
+  document.getElementById("audio3").play();
+}
